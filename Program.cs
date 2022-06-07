0bf05da2d9bebfd38e45f3e19136ad03b1a2a97f
Coronel Francisco Exequiel <execoronel14f@gmail.com>
2022-06-07 14:53:33 -0300

    Archivos dentro de la carpeta listados

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese el path de la carpeta:");
+string path= Console.ReadLine();
+if (Directory.Exists(path))
+{
+    List<string> archivos = new List<string>();
+    archivos = Directory.GetFiles(path).ToList();
+    Console.WriteLine("Estos son los archivos que se encuentran dentro del path:");
+    foreach (string item in archivos)
+    {
+        Console.WriteLine(item);
+    }
+} else
+{
+    Console.WriteLine("La ruta ingresada no existe");
+}
